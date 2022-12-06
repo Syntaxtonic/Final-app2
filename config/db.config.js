cfc8c5a5f21e248b8113630bb1f18c2ed9cf905c
cishebusisiwe <cishebusisiwe@gmail.com>
2022-12-06 10:36:13 +0200

    adding mongodb atlas

--- config/db.config.js
+++ config/db.config.js
@@ -1,2 +1,4 @@
 module.exports = {
-    HOST: "localhost", PORT: 27017, DB: "Nolingo_db"};
+    // HOST: "localhost", PORT: 27017, DB: "Nolingo_db"
+    url: 'mongodb+srv://Melikhaya:password1234@cluster0.jciocgb.mongodb.net/PhilippiLearning?retryWrites=true&w=majority'
+};
